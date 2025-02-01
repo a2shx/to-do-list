9102e60ed9d62087caaf89aa54cc459648143a8f
Chonrawee Charoenritichai <atx211me@gmail.com>
2025-02-01 23:01:25 +0700

    update
    feat: finished task function
    style: recolor unfinish, finished task

--- script.js
+++ script.js
@@ -2,84 +2,101 @@ const addTodoButton = document.querySelector('.addButton');
 const taskList = document.querySelector('.task-list');
 const taskInput = document.querySelector('#task-input');
 const addingStatus = document.querySelector('.adding-status');
+const finishedTaskList = document.querySelector('.finished-task-list');
+let statusTimeout;
 
 addTodoButton.addEventListener('click', () => {
+    createTaskBox();
+})
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter'){
+        createTaskBox();
+    }
+})
+function createTaskBox(){
     if(taskInput.value != ''){
-    const taskBox = document.createElement('div');
-    taskBox.classList.add('task-box');
-//CREATE LEFT SIDE OF TASKBOX
-        const leftTask = document.createElement('div');
-        leftTask.classList.add('left-task');
-
-            const check = document.createElement('i');
-            check.classList.add('fa-solid','fa-circle-check');
-
-            const taskTitle = document.createElement('input');
-            taskTitle.value = taskInput.value;
-            taskTitle.classList.add('edit-input');
-            taskTitle.type = 'text';
-            taskTitle.setAttribute('disabled',true);
-
-        leftTask.append(check, taskTitle);
-//CREATE RIGHT SIDE OF TASKBOX
-        const rightTask = document.createElement('div');
-        rightTask.classList.add('right-task');
-
-            const editTask = document.createElement('i');
-            editTask.classList.add('fa-solid','fa-pencil','edit-task');
-            editTask.addEventListener('click', () => {
-                taskTitle.removeAttribute('disabled');
-                taskTitle.focus();
-
-                document.addEventListener('click', (e) => {
-                    if (e.target !== taskTitle && e.target !== editTask) {
-                        taskTitle.setAttribute('disabled', true);
-                    }
+        const taskBox = document.createElement('div');
+        taskBox.classList.add('task-box','green-bg');
+    //CREATE LEFT SIDE OF TASKBOX
+            const leftTask = document.createElement('div');
+            leftTask.classList.add('left-task');
+    
+                const check = document.createElement('i');
+                check.classList.add('fa-solid','fa-circle-check');
+    
+                const taskTitle = document.createElement('input');
+                taskTitle.value = taskInput.value;
+                taskTitle.classList.add('edit-input');
+                taskTitle.type = 'text';
+                taskTitle.setAttribute('disabled',true);
+    
+            leftTask.append(check, taskTitle);
+    
+    //CREATE RIGHT SIDE OF TASKBOX
+            const rightTask = document.createElement('div');
+            rightTask.classList.add('right-task');
+    
+                const editTask = document.createElement('i');
+                editTask.classList.add('fa-solid','fa-pencil','edit-task');
+                editTask.addEventListener('click', () => {
+                    taskTitle.removeAttribute('disabled');
+                    taskTitle.focus();
+    
+                    document.addEventListener('click', (e) => {
+                        if (e.target !== taskTitle && e.target !== editTask) {
+                            taskTitle.setAttribute('disabled', true);
+                        }
+                    })
+                    document.addEventListener('keydown', (e) => {
+                        if(e.key === 'Enter'){
+                            taskTitle.setAttribute('disabled', true);
+                        }
+                    })
+    
                 })
-                document.addEventListener('keydown', (e) => {
-                    if(e.key === 'Enter'){
-                        taskTitle.setAttribute('disabled', true);
-                    }
+               
+                const removeTask = document.createElement('i');
+                removeTask.classList.add('fa-solid','fa-trash','remove-task');
+                removeTask.addEventListener('click', () => {
+                    taskBox.remove();
+                    showAddingStatus('Task removed!', 'red-font');
                 })
-
-            })
-           
-
-
-            const removeTask = document.createElement('i');
-            removeTask.classList.add('fa-solid','fa-trash','remove-task');
-            removeTask.addEventListener('click', () => {
-                taskBox.remove();
-                addingStatus.classList.remove('green-font');
-                addingStatus.classList.add('red-font');
-                addingStatus.innerHTML = 'Task Removed!';
-                setTimeout( () => {
-                    addingStatus.innerHTML = '';
-                }, 3000);
-            })
-
-        rightTask.append(editTask, removeTask);
-
-    taskBox.append(leftTask, rightTask);
-//TASK ADDING STATUS
-    taskList.append(taskBox);
-        addingStatus.classList.remove('red-font');
-        addingStatus.classList.add('green-font');
-        addingStatus.innerHTML = 'Task Added!'
-        setTimeout( () => {
-            addingStatus.innerHTML = '';
-        }, 3000);
-//REMOVE FORM'S VALUE
-        taskInput.value = '';
-    }
-    else{
-        const addingStatus = document.querySelector('.adding-status');
-        addingStatus.classList.remove('green-font');
-        addingStatus.classList.add('red-font');
-        addingStatus.innerHTML = 'You need to fill something!'
-        setTimeout( () => {
-            addingStatus.innerHTML = '';
-        }, 3000);
-    }
+    
+            rightTask.append(editTask, removeTask);
+    
+        taskBox.append(leftTask, rightTask);
+    //TASK ADDING STATUS
+        taskList.append(taskBox);
+        showAddingStatus('Task added!', 'green-font');
+    
+    //REMOVE FORM'S VALUE
+            taskInput.value = '';
+    
+    //FINISH TASK
+    
+        check.addEventListener('click', () => {
+            check.remove();
+            editTask.remove();
+            taskBox.classList.remove('green-bg');
+            taskBox.classList.add('pink-bg');
+            finishedTaskList.append(taskBox);
+            showAddingStatus('Task finished!', 'green-font')
+        });
+    
+        }
+        else{
+            const addingStatus = document.querySelector('.adding-status');
+            showAddingStatus('You need to fill something!', 'red-font');
+        }
 }
-)
+function showAddingStatus(message, classname) {
+
+    clearTimeout(statusTimeout);
+    addingStatus.classList.remove('green-font', 'red-font');
+    addingStatus.classList.add(classname);
+    addingStatus.innerHTML = message;
+
+    statusTimeout = setTimeout( () => {
+        addingStatus.innerHTML = '';
+    }, 3000);
+}
