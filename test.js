3d3ddb68c68712d951261e790e993cb9f1719a35
Chonrawee Charoenritichai <atx211me@gmail.com>
2025-02-18 11:03:55 +0700

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const taskTitle = document.querySelector('.edit-input')
-const addButton = document.querySelector('.addButton')
-
-document.addEventListener('keydown', (e) => {
-    console.log(e.key);
-})
